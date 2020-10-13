13ba6801ef05272665c45c9448071bc0ad37ca19
Ronald Z <xrdmzx@gmail.com>
2020-10-13 17:11:47 -0400

    Initial Commit

---
+++ GenE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GenE
+//
+//  Created by Ronald Zambrano on 10/13/20.
+//  Copyright © 2020 Group3b. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
